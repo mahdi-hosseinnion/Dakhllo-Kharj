944d6dccef8f3d40a1cd1f8bf53368f6a32e024d
mahdi-hosseinnion <45493154+mahdi-hosseinnion@users.noreply.github.com>
2020-05-25 20:55:07 +0330

    add RecyclerView

--- app/src/main/java/com/example/dakhllokharj/MainActivity.java
+++ app/src/main/java/com/example/dakhllokharj/MainActivity.java
@@ -3,28 +3,59 @@ package com.example.dakhllokharj;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 
+import com.example.dakhllokharj.adapter.ReceiptRecyclerAdapter;
 import com.example.dakhllokharj.database.MyDataBase;
 import com.example.dakhllokharj.database.Receipt;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     //var
     private static final String TAG = "MainActivity";
     MyDataBase myDataBase;
+    private List<Receipt> mReceipt = new ArrayList<>();
+    private ReceiptRecyclerAdapter mAdapter;
     //ui component
+    private RecyclerView recycler_receipt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+        init();
 
     }
-    private void init(){
-        myDataBase=MyDataBase.getInstance(this);
+
+    private void init() {
+        myDataBase = MyDataBase.getInstance(this);
+        //init recycler
+        mAdapter = new ReceiptRecyclerAdapter(this, mReceipt);
+        recycler_receipt.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recycler_receipt.setAdapter(mAdapter);
+        myDataBase.dao().getAllReceipt().observe(this, new Observer<List<Receipt>>() {
+            @Override
+            public void onChanged(List<Receipt> receipts) {
+                if (mReceipt.size() > 0) {
+                    mReceipt.clear();
+                }
+                if (receipts != null) {
+                    mReceipt.addAll(receipts);
+                }
+
+            }
+        });
+    }
+
+    private void findViews() {
+        recycler_receipt = findViewById(R.id.recycler_receipt);
 
     }
 }
---
+++ app/src/main/java/com/example/dakhllokharj/adapter/ReceiptRecyclerAdapter.java
@@ -0,0 +1,57 @@
+package com.example.dakhllokharj.adapter;
+
+import android.content.Context;
+import android.view.ContextThemeWrapper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.dakhllokharj.R;
+import com.example.dakhllokharj.database.Receipt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ReceiptRecyclerAdapter extends RecyclerView.Adapter<ReceiptRecyclerAdapter.ViewHolder>{
+    private Context context;
+    private List<Receipt> data;
+
+    public ReceiptRecyclerAdapter(Context context, List<Receipt> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.item_transcation, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Receipt receipt=data.get(position);
+        holder.txt_title.setText(receipt.getTitle());
+        holder.txt_amount.setText(""+receipt.getAmount());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView txt_title,txt_amount;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txt_title = itemView.findViewById(R.id.txt_title);
+            txt_amount = itemView.findViewById(R.id.txt_amount);
+
+        }
+    }
+}
